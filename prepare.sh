78ac8986f1a149c13ac32d426ad724e2eb4e403b
Dmitry Andreev <dmitry.andreev@latoken.com>
2020-07-15 23:52:22 +0300

    fixed prepare.sh script

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,7 @@
 gdown https://drive.google.com/uc?id=1badu11NqxGf6qM3PTTooQDJvQbejgbTv
+unzip CelebAMask-HQ.zip
 python3 prepare_data/prepare_CelebAMask.py
 
 wget http://projects.i-ctm.eu/sites/default/files/AltroMateriale/207_Michele%20Svanera/Figaro1k.zip
+unzip Figaro1k.zip
 python3 prepare_data/prepare_Figaro_1K.py
