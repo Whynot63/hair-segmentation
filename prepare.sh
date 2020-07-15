fe15fd1337fd0953443a3b8eec942108bb6fb559
Dmitry Andreev <dmitry.andreev@latoken.com>
2020-07-15 23:43:45 +0300

    added script for prepare full dataset

---
+++ prepare.sh
@@ -0,0 +1,5 @@
+gdown https://drive.google.com/open?id=1badu11NqxGf6qM3PTTooQDJvQbejgbTv
+python3 prepare_data/prepare_CelebAMask.py
+
+wget http://projects.i-ctm.eu/sites/default/files/AltroMateriale/207_Michele%20Svanera/Figaro1k.zip
+python3 prepare_data/prepare_Figaro_1K.py
